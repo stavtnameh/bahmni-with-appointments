f83f039a3cce00ce2c6cd6501314da114d473243
stavtnameh <hemanth.v@beehyv.com>
2022-06-30 18:05:41 +0530

    Changed Copy Path

--- openmrs_appointments/check-omod.sh
+++ openmrs_appointments/check-omod.sh
@@ -2,4 +2,4 @@
 git clone https://github.com/Bahmni/openmrs-module-appointments
 cd openmrs-module-appointments
 mvn clean package
-cp -rf omod/target/appointments-*-SNAPSHOT.omod .OpenMRS/modules/
+cp -rf omod/target/appointments-*-SNAPSHOT.omod ../.OpenMRS/modules/
